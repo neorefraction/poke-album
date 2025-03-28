5f06adfb87b90bef7201cfa80bdb77ef376cfdfb
Johnny Astudillo Soriano <neorefraction@Johnnys-Air.technicolor.net>
2025-03-28 18:19:27 +0100

    Authentication endpoints created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import authRouter from './routers/authRouter.js';
 
+// Creates an express app
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Enables json requests
+app.use(express.json());
+
+// Routes
+app.use('/auth', authRouter);
+
+// Server start
 app.listen(3000, () => {
-    console.log('Server started up 3000');
+    console.log(`Server started up ${PORT}`);
 })
